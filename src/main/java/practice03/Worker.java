1bca83e4afde84eac073c43d766f89cfa3f8d0c9
zzp <2269225970@qq.com>
2018-04-10 19:10:09 +0800

    override method introduce

--- src/main/java/practice03/Worker.java
+++ src/main/java/practice03/Worker.java
@@ -1,4 +1,9 @@
 package practice03;
 
-public class Worker {
+public class Worker extends Person{
+
+    @Override
+    public String introduce() {
+        return  "I am a Worker. I have a job.";
+    }
 }
