f6bc41f169d5e30fdea2f60046650d5aaecfb243
zzp <2269225970@qq.com>
2018-04-10 19:14:43 +0800

    add constructor function

--- src/main/java/practice03/Worker.java
+++ src/main/java/practice03/Worker.java
@@ -2,6 +2,10 @@ package practice03;
 
 public class Worker extends Person{
 
+    public Worker(String name, int age) {
+        super(name, age);
+    }
+
     @Override
     public String introduce() {
         return  "I am a Worker. I have a job.";
