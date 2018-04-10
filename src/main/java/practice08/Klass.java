fecb750604f01b3d23c899e65a09bbc7225cadd9
zzp <2269225970@qq.com>
2018-04-10 20:55:23 +0800

    complete

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,27 @@
 package practice08;
 
 public class Klass {
+    private int number;
+    private Student leader;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getDisplayName() {
+        return "Class " + number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void assignLeader(Student leader) {
+        this.leader = leader;
+    }
 }
+
