bcc757429bbaa7d3adebd0fe688dd2be5dcd2318
zzp <2269225970@qq.com>
2018-04-10 19:15:20 +0800

    complete

--- src/main/java/practice03/Person.java
+++ src/main/java/practice03/Person.java
@@ -1,4 +1,42 @@
 package practice03;
 
 public class Person {
+    private int age;
+    private  String name;
+
+    public Person() {
+    }
+
+    public Person(String name, int age) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String introduce(){
+        return "My name is " + name + ". I am " + age + " years old.";
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
