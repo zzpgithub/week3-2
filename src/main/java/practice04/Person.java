278f1508a278ccd6e604993f2c5f15958164f85d
zzp <2269225970@qq.com>
2018-04-10 19:26:33 +0800

    complete

--- src/main/java/practice04/Person.java
+++ src/main/java/practice04/Person.java
@@ -1,4 +1,34 @@
 package practice04;
 
 public class Person {
+    private int age;
+    private  String name;
+
+    public Person() {
+    }
+
+    public Person(String name, int age) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String introduce(){
+        return "My name is " + name + ". I am " + age + " years old.";
+    }
 }
