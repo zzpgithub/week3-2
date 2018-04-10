37b6e69667ab6da4640893b1262633dacf86c9b7
zzp <2269225970@qq.com>
2018-04-10 19:45:01 +0800

    complete

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,26 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+    private int klass;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+    public int getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        if (klass == 0 ) {
+            return super.introduce() + " I am a Teacher. I teach No Class.";
+        } else {
+            return super.introduce() + " I am a Teacher. I teach Class " + klass + ".";
+        }
+    }
 }
